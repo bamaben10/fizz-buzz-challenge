e444009c731a7e6b338335befd082ccd97b7e884
Benjamin Salmon <bensalmon8@gmail.com>
2022-10-17 18:36:48 -0500

    loop practice

--- challenge-fizz-buzz/fizzBuzz.js
+++ challenge-fizz-buzz/fizzBuzz.js
@@ -14,6 +14,20 @@
 //     }
 // }
 
+// for (let i = 1; i < 51; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("fizzbuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("buzz");
+//   } else if (i % 15 === 0) {
+//     console.log("fizzbuzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
 for (let i = 1; i < 51; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("fizzbuzz");
