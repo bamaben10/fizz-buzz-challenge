b8489baae87f95b13ac82878190d0828705c301e
Benjamin Salmon <bensalmon8@gmail.com>
2023-01-06 16:28:05 -0600

    practice problems

--- challenge-fizz-buzz/fizzBuzz.js
+++ challenge-fizz-buzz/fizzBuzz.js
@@ -16,16 +16,16 @@ for (let i = 1; i < 51; i++) {
   }
 }
 
-// for(let i = 1; i < 51; i ++) {
-//     if(i % 3 === 0 && i % 5 === 0) {
-//         console.log('fizzbuzz')
-//     }    else if(i % 5 === 0) {
-//         console.log('buzz')
-//     }   else if(i % 3 === 0){
-//         console.log('fizz')
-//     }   else if (i % 15 === 0) {
-//         console.log('fizzbuzz')
-//     }   else {
-//         console.log(i)
-//     }
-// }
+for (let i = 1; i < 51; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else if (i % 15 === 0) {
+    console.log("fizzbuzz");
+  } else {
+    console.log(i);
+  }
+}
